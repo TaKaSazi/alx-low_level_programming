0eb92e645438d8354322b56b1beaa8c0bab855ae
Sibusiso <sibusisontja85@.gail.com>
2023-11-02 04:13:49 -0700

    0-isuper

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -2,16 +2,19 @@
 
 /**
  * _isupper - check if a letter is upper
- * @n: the number to be checked
+ * @c: input character
  * Return: 1 for upper letter or 0 for any size
  */
 
-int _isupper(int x)
+int _isupper(int c)
 {
-	if (x >= 65 && x <= 90)
+	if (c >= 'A' && c <= 'Z')
 	{
-	return (1);
+		return (1);
 	}
-	return (0);
+	else
+	{
+		return (0);
+	}
 
 }
