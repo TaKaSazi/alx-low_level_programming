45ae9f7d0edbf1a43933c7a5336795584425cd52
Sibusiso <sibusisontja85@.gail.com>
2023-11-08 23:31:17 -0800

    0-strcat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -8,21 +8,18 @@
  */
 char *_strcat(char *dest, char *src)
 {
-	int i;
-	int j;
+	int  dlen = 0, i;
+	
+	while (dest[dlen])
+	{
+	dlen++;
+	}
 
-	i = 0;
-	while (dest[i] != '\0')
+	for (i = 0; src[i] != 0; i++)
 	{
-		i++;
+		dest[dlen] = src[i];
+		dlen++;
 	}
-	j = 0;
-	while (src[j] != '\0')
-	{
-		dest[i] = src[j];
-		i++;
-		j++;
-	}
-	dest[i] = '\0';
+	dest[dlen] = '\0';
 	return (dest);
 }
