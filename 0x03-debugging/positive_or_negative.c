da3b04901c9243c435a425c46ae4dff064805fb6
Sibusiso <sibusisontja85@.gail.com>
2023-11-12 14:59:03 -0800

    102-magic

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "main.h"
+
+void positive_or_negative(int i)
+{
+	if (i > 0)
+		printf("%d is positive\n", f);
+	else if (i == 0)
+		printf("%d is zero\n", f);
+	else
+		printf("%d is negative\n", f);
+} 
