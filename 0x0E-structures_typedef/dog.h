dee07a2dd776b9d522bfa71e42ebdb363a9a6b3a
Sibusiso <sibusisontja85@.gail.com>
2023-11-27 12:40:34 -0800

    dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+/**
+ * struct dog - a string that create a dog info
+ * @name: the name of the dog
+ * @age: the age of the dog
+ * @owner: owner of the dog
+ * Description: first struct with alx
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
